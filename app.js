ef7540b250916423735b2bff4ce8230a6298bc8a
Ivan Ganchev <iganchev@nemetschek.bg>
2023-12-24 15:41:09 +0200

    run monogdb

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const session = require('express-session');
 const allRoutes = require('./routes');
 
 const app = express();
 app.use(express.json());
 app.use(session({ secret: 'mySecret', resave: false, saveUninitialized: false }));
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 app.use('/', allRoutes);
 
