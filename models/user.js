b5fe0b5cca63cc5e9db98ed347e9792d8503a359
Ivan Ganchev <iganchev@nemetschek.bg>
2023-12-24 13:34:50 +0200

    user model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+UserSchema.statics.hashPassword = function(password) {
+    return bcrypt.hash(password, 10);
+};
+
+UserSchema.methods.comparePassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
