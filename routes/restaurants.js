b5975501bb0ceff26ac749fa3dd4b24fbc45ee78
Ivan Ganchev <iganchev@nemetschek.bg>
2024-12-14 19:14:38 +0200

    ignore returning db generated field

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -74,7 +74,7 @@ router.post('/create', verifyToken, jsonParser, upload.single('image'), async (r
 
 router.get('/', verifyToken, async (req, res) => {
     try {
-        const restaurants = await Restaurant.find().select('-_id');
+        const restaurants = await Restaurant.find().select('-_id').select('-__v');
         res.status(200).json(restaurants);
     } catch (error) {
         console.error('Error fetching restaurants:', error);
